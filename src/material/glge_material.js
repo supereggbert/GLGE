d755406752b9760b2674d5e3267e9fb854a50f0b
Paul Brunt <me@paulbrunt.co.uk>
2011-03-12 08:36:31 +0000

    fix material alpha issue

--- src/material/glge_material.js
+++ src/material/glge_material.js
@@ -727,7 +727,7 @@ GLGE.Material.prototype.getFragmentShader=function(lights,colors){
 			var txcoord="xyz";
 			var sampletype="Cube";
 		}
-		shader=shader+"al = al*(1.0-mask) + texture"+sampletype+"(TEXTURE"+this.layers[diffuseLayer].texture.idx+", textureCoords."+txcoord+").a*mask;\n";        
+		shader=shader+"al = al*(1.0-mask) + texture"+sampletype+"(TEXTURE"+this.layers[diffuseLayer].texture.idx+", textureCoords."+txcoord+").a*al*mask;\n";        
 	}
 	if(this.binaryAlpha) {
 		shader=shader+"if(al<0.5) discard;\n";
